b08fb01699dac36dacd3f4a7ba76ab56321d2482
Pete Petrash <pete.petrash@gmail.com>
2015-10-12 19:36:11 -0700

    cleaning up class names

--- app/components/Grid/Grid.jsx
+++ app/components/Grid/Grid.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default class Grid extends React.Component {
+export default class Grid extends React.Comnent {
     render() {
         return(
             <div>
                 <h1>Grid</h1>
-                    <section className="GridContainer">
+                    <section className="container">
                         <div className="column"></div>
                         <div className="column"></div>
                         <div className="column"></div>
