491fc957beabe55910ada986c944217fb2544b95
Pete Petrash <pete.petrash@gmail.com>
2015-10-12 19:37:47 -0700

    oops.

--- app/components/Grid/Grid.jsx
+++ app/components/Grid/Grid.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Grid extends React.Comnent {
+export default class Grid extends React.Component {
     render() {
         return(
             <div>
