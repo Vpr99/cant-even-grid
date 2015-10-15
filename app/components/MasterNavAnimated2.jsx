c00d2b552a71671833eec483c405d9a71d07bd1b
Pete Petrash <pete.petrash@gmail.com>
2015-10-15 12:31:44 -0700

    progress, at last

--- app/components/MasterNavAnimated2.jsx
+++ app/components/MasterNavAnimated2.jsx
@@ -6,44 +6,51 @@ import { Link } from 'react-router';
 
 const MasterNavAnimated2 = React.createClass({
     getInitialState() {
-        return {x: 0};
+        return {x: 0, opacity: 0};
     },
     componentDidMount() {
-        this.reveal();
+        this.setState({
+            x: spring(0, [1000, 80]),
+            opacity: spring(1, [1000, 80])}
+        );
     },
-    reveal() {
-        this.setState({x: 100});
+    handleMouseDown() {
+        console.log('mouseDown');
     },
     getStyles(prevStyles) {
         // `prevStyles` is the interpolated value of the last tick
         const endValue = prevStyles.map((_, i) => {
             return i === 0 ? this.state: {
-                x: spring(prevStyles[i - 1].x, [150, 50])
+                x: spring(prevStyles[i - 1].x, [1000, 80]),
+                opacity: spring(prevStyles[i - 1].opacity, [1000, 80])
             };
         });
         return endValue;
     },
     render() {
         return (
-            <div className="MasterNav">
-                <StaggeredMotion defaultStyles={range(7).map(() => ({x: 0}))}
-                styles={this.getStyles}>
-                    {listItems =>
-                        <li className="wut">
-                            {listItems.map(({x}, i) =>
-                            <Link
-                                to="/typography"
-                                className="pageLink"
+            <StaggeredMotion defaultStyles={range(7).map(() => ({x: -100, opacity: 0}))} styles={this.getStyles}>
+                {navListItems =>
+                    <div className="MasterNav" >
+                        {navListItems.map(({x, opacity}, i) =>
+                            <li className="navListItem"
                                 key={i}
-                                style={{ transform: `translateX(${x + 0}px)`}}>{x}
-                            </Link>
-                             )}
-                    </li>
-                }
+                                style={{
+                                    opacity: `${opacity}`,
+                                    transform: `translateX(${x + 0}px)`
+                                }}>
+                                <Link
+                                    to="/"
+                                    className="navLink" >
+                                    {x}
+                                </Link>
+                            </li>
+                         )}
+                     </div>
+                 }
             </StaggeredMotion>
-        </div>
-    );
-},
+        );
+    },
 });
 
 export default MasterNavAnimated2;
