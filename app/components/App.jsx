5728f5b55406e22886761e706206b50fe6575280
Pete Petrash <pete.petrash@gmail.com>
2015-10-15 12:50:17 -0700

    animationz

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { IndexRoute, Router, Route } from 'react-router'
 import MasterNavAnimated2 from './MasterNavAnimated2'
+import Demo from './Demo'
 
 let App = React.createClass({
     render() {
         return (
             <div className="App">
+                <Demo />
                 <section className="MasterDetailGrid">
                     <MasterNavAnimated2 />
                     <div className="Detail">
