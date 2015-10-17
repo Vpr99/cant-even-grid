aa8d4a0d7796ff7f894afb7b74472961b82e1918
Pete Petrash <pete.petrash@gmail.com>
2015-10-16 21:59:18 -0700

    adding some more grids

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,12 +4,14 @@ import { TransitionMotion, spring } from 'react-motion'
 import RouteTransition from './RouteTransition'
 import _ from 'lodash';
 import range from 'lodash/utility/range';
+import Demo from './Demo';
 
 let App = React.createClass({
     render() {
         return <div className="App">
             <section className="MasterDetailGrid">
                 <MasterNav />
+                <Demo />
                 <div className="Detail">
                     <RouteTransition pathname={this.props.location.pathname} defaultStyles={{opacity: 0, scale: 0}}>
                         {this.props.children}
--- app/components/Grid/Grid.jsx
+++ app/components/Grid/Grid.jsx
@@ -4,11 +4,33 @@ export default class Grid extends React.Component {
     render() {
         return(
             <div>
-                <h1>Grid</h1>
-                <section className="grid-one">
+                <h1>Grid System</h1>
+                <h2>Grid sizes</h2>
+                <h3>Size 1 of 1</h3>
+                <section className="grid-example grid-example-1">
+                    <div></div>
+                </section>
+                <h3>4 column</h3>
+                <section className="grid-example grid-example-2">
+                    <div></div>
+                    <div></div>
+                </section>
+                <h3>8 column</h3>
+                <section className="grid-example grid-example-4">
                     <div></div>
                     <div></div>
                     <div></div>
+                    <div></div>
+                </section>
+                <section className="grid-example grid-example-6">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </section>
+                <section className="grid-example grid-example-9">
                     <div></div>
                     <div></div>
                     <div></div>
